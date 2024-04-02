9985e794dc9c80d4e5840e72bd35b611e4bb5a30
Maxime <maximechagnon00@gmail.com>
2024-04-02 10:48:41 +0200

    feat: `ScrabbleApplicationConsole`

---
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -0,0 +1,11 @@
+package scrabble.application;
+
+public class ScrabbleApplicationConsole {
+
+	public static void main(String[] args) {
+		System.out.println("--------------------------------------------");
+		System.out.println("-- Bienvenue dans notre jeu de scrabble ! --");
+		System.out.println("--------------------------------------------");
+	}
+
+}

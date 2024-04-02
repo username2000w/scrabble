d0b3f73c4b7596a308a2385b8965a0141543cfa4
asalmon16 <alexis.salmon@etu.unilim.fr>
2024-04-02 11:17:39 +0200

    feat: description

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -3,9 +3,9 @@ package scrabble.application;
 public class ScrabbleApplicationConsole {
 
 	public static void main(String[] args) {
-		System.out.println("--------------------------------------------");
-		System.out.println("-- Bienvenue dans notre jeu de scrabble ! --");
-		System.out.println("--------------------------------------------");
+		System.out.println("-------------------------------------------------------");
+		System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
+		System.out.println("-- développé par Maxime, Mikkel, Léo et Alexis      --");
+		System.out.println("-------------------------------------------------------");
 	}
-
 }

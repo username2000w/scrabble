e683b6b01bec0ff2b8504403c5bb7436dfc57b71
iut <iut@10.0.2.15>
2024-04-08 14:12:33 +0200

    feat: ajout classe Chevalet

---
+++ src/main/java/model/Chevalet.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+import scrabble.model.Tuile;
+
+public class Chevalet {
+	ArrayList<Tuile> tuiles = new ArrayList<Tuile>();
+	
+	public Chevalet(ArrayList<Tuile> tuiles) {
+		this.tuiles = tuiles;
+	}
+	
+	public void piocher() {
+		//TODO
+	}
+}

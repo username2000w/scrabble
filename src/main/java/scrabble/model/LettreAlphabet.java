2a264ec74f7ae315096a8cc05a2ba8e4e8ca8cf5
Maxime <maximechagnon00@gmail.com>
2024-04-12 18:00:52 +0200

    feat: points pour les lettres

--- src/main/java/scrabble/model/LettreAlphabet.java
+++ src/main/java/scrabble/model/LettreAlphabet.java
@@ -1,30 +1,40 @@
 package scrabble.model;
 
 public enum LettreAlphabet {
-  A,
-  B,
-  C,
-  D,
-  E,
-  F,
-  G,
-  H,
-  I,
-  J,
-  K,
-  L,
-  M,
-  N,
-  O,
-  P,
-  Q,
-  R,
-  S,
-  T,
-  U,
-  V,
-  W,
-  X,
-  Y,
-  Z
+  A(1),
+  B(3),
+  C(3),
+  D(2),
+  E(1),
+  F(4),
+  G(2),
+  H(4),
+  I(1),
+  J(8),
+  K(10),
+  L(1),
+  M(2),
+  N(1),
+  O(1),
+  P(3),
+  Q(8),
+  R(1),
+  S(1),
+  T(1),
+  U(1),
+  V(4),
+  W(10),
+  X(10),
+  Y(10),
+  Z(10);
+
+  private int points;
+  
+  private LettreAlphabet(int points) {
+    this.points = points;
+  }
+  
+  public int getPoints() {
+	return this.points;
+  }
 }

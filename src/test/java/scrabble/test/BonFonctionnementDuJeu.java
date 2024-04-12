ad5f51ece126a5af9bbac5d5ea6eadfe6640f4ba
Maxime <maximechagnon00@gmail.com>
2024-04-12 20:21:23 +0200

    feat: premiers test pour Chevalet

---
+++ src/test/java/scrabble/test/BonFonctionnementDuJeu.java
@@ -0,0 +1,34 @@
+package scrabble.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import scrabble.model.Chevalet;
+import scrabble.model.Sac;
+
+class BonFonctionnementDuJeu {
+	Sac sac;
+	Chevalet chevalet;
+
+	@BeforeEach
+	void init() {
+		sac = new Sac();
+		chevalet = new Chevalet(sac);
+	}
+	
+	@Test
+	void chevaletVide() {
+		assertEquals(true, chevalet.getTuiles().isEmpty());
+	}
+	
+	@Test
+	void pioche() {
+		int taille = chevalet.getTuiles().size();
+		chevalet.piocher();
+		
+		assertEquals(taille + 1, chevalet.getTuiles().size());
+	}
+
+}

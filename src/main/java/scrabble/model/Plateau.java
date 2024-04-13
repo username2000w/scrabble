ce9af5063cd48f7e2db0e329a4119e182850547a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-13 18:39:22 +0200

    feat(Plateau): ajout méthode `afficher()`

--- src/main/java/scrabble/model/Plateau.java
+++ src/main/java/scrabble/model/Plateau.java
@@ -5,9 +5,33 @@ public class Plateau {
 	
 	public Plateau() {
 		for (int i = 0; i < 15; i++) {
-		    for (int j = 0; j < 15; j++) {
-		        plateau[i][j] = new Case();
-		    }
+      for (int j = 0; j < 15; j++) {
+        plateau[i][j] = new Case();
+      }
 		}
 	}
+
+  public void afficher() {
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 15; j++) {
+        // On a une étoile sur la case du milieu.
+        if (j == 7 && i == 7) {
+          System.out.print("★ ");
+          continue;
+        }
+
+        LettreAlphabet lettre = plateau[i][j].getLettre();
+
+        if (lettre == null) { // si la case est vide, on affiche un carré vide.
+          System.out.print("□ ");
+        }
+        else {
+          System.out.print(lettre + " ");
+        }
+      }
+
+      // Retour à la ligne.
+      System.out.println();
+    }
+  }
 }

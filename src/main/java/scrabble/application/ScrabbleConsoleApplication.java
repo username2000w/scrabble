2f88c07bed0b069db36085ad787782b06e1de523
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-13 18:07:24 +0200

    refactor: nom de la classe de l'application

--- src/main/java/scrabble/application/ScrabbleConsoleApplication.java
+++ src/main/java/scrabble/application/ScrabbleConsoleApplication.java
@@ -1,11 +1,11 @@
 package scrabble.application;
 
-public class ScrabbleApplicationConsole {
+public class ScrabbleConsoleApplication {
 
 	public static void main(String[] args) {
 		System.out.println("-------------------------------------------------------");
 		System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
-		System.out.println("-- développé par Maxime, Mikkel, Léo et Alexis      --");
+		System.out.println("-- développé par Maxime, Mikkel, Léo et Alexis       --");
 		System.out.println("-------------------------------------------------------");
 	}
 }

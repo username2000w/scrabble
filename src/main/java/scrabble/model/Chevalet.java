786245050fd5c109e197b26cf26a8a3605d7089d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-13 19:13:56 +0200

    feat(Chevalet): ajout distribution de 7 tuiles dans le constructeur

--- src/main/java/scrabble/model/Chevalet.java
+++ src/main/java/scrabble/model/Chevalet.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 
 
 public class Chevalet {
-	private ArrayList<Tuile> tuiles = new ArrayList<Tuile>();
+	private ArrayList<Tuile> tuiles;
 	private Sac sac;
 	
 	public Chevalet(Sac sac) {
+    this.tuiles = new ArrayList<>();
 		this.sac = sac;
+    
+    // On distribue 7 tuiles.
+    for (int i = 0; i < 7; i++) {
+      this.piocher();
+    }
 	}
 	
 	public void piocher() {

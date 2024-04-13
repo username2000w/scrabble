3874e8a5018e195956ccb9cc463ecf229d96078a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-13 19:35:41 +0200

    feat(Chevalet): afficherTuiles et init des tuiles dans Joueur

--- src/main/java/scrabble/model/Chevalet.java
+++ src/main/java/scrabble/model/Chevalet.java
@@ -2,7 +2,6 @@ package scrabble.model;
 
 import java.util.ArrayList;
 
-
 public class Chevalet {
 	private ArrayList<Tuile> tuiles;
 	private Sac sac;
@@ -10,29 +9,31 @@ public class Chevalet {
 	public Chevalet(Sac sac) {
     this.tuiles = new ArrayList<>();
 		this.sac = sac;
-    
-    // On distribue 7 tuiles.
-    for (int i = 0; i < 7; i++) {
-      this.piocher();
-    }
 	}
 	
-	public void piocher() {
+	public void piocher(int tuileIndex) {
 		if (this.tuiles.size() < 7) {
-			this.tuiles.add(this.sac.piocher());	
+			this.tuiles.add(tuileIndex, this.sac.piocher());	
 		}			
 	}
 	
-	public void echanger(Tuile tuile) {
-		this.tuiles.remove(tuile);
+	public void echanger(int tuileIndex) {
+		Tuile tuile = this.tuiles.remove(tuileIndex);
 		this.sac.ajouter(tuile);
 	}
 	
-	public Tuile getTuileAvecIndex(int index) {
-		return this.tuiles.get(index);
+	public Tuile getTuileAvecIndex(int tuileIndex) {
+		return this.tuiles.get(tuileIndex);
 	}
 
 	public ArrayList<Tuile> getTuiles() {
 		return this.tuiles;
 	}
+
+  public void afficherTuiles() {
+    for (int i = 0; i < 7; i++) {
+      System.out.print(this.getTuileAvecIndex(i).getLettre() + " ");
+    }
+    System.out.print("\n");
+  }
 }

148528b24460234443a4c2ddb81a177b9092462e
Léo Gaborit <leo.gaborit@etu.unilim.fr>
2024-04-30 11:35:36 +0200

    Affichage des coordonées sur le Plateau

--- src/main/java/scrabble/model/Plateau.java
+++ src/main/java/scrabble/model/Plateau.java
@@ -1,51 +1,37 @@
 package scrabble.model;
 
-import scrabble.model.utils.exception.HorsPlateauException;
-
 public class Plateau {
-	private static final int TAILLE_PLATEAU_HORIZONTALE = 15;
-	private static final int TAILLE_PLATEAU_VERTICALE = 15;
-	
-	private Case[][] plateau = new Case[TAILLE_PLATEAU_VERTICALE][TAILLE_PLATEAU_HORIZONTALE]; 
+	private Case[][] plateau = new Case[15][15]; 
 	
 	public Plateau() {
-		for (int i = 0; i < TAILLE_PLATEAU_VERTICALE; i++) {
-	      for (int j = 0; j < TAILLE_PLATEAU_HORIZONTALE; j++) {
-	    	  if (j == 7 && i == 7) {
-	    		  plateau[i][j] = new Case(Bonus.ETOILE);
-	            }
-	    	  else {
-	    		  plateau[i][j] = new Case(null);  
-	    	  }
-	      }
+		for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 15; j++) {
+        plateau[i][j] = new Case();
+      }
 		}
 	}
 
   public void afficher() {
-    for (int i = 0; i < TAILLE_PLATEAU_VERTICALE; i++) {
-      for (int j = 0; j < TAILLE_PLATEAU_HORIZONTALE; j++) {
-        LettreAlphabetFrancais lettre = plateau[i][j].getLettre();
-        Bonus bonus = plateau[i][j].getBonus();
-        
-        if (bonus == null) {
-        	if (lettre == null) { // si la case est vide, on affiche un carré vide.
-        		System.out.print("□ ");
-        	}
-        	else {
-        		System.out.print(lettre + " ");
-        	}
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 15; j++) {
+        // On a une étoile sur la case du milieu.
+        if (j == 7 && i == 7) {
+          System.out.print("★ ");
+          continue;
+        }
+
+        LettreAlphabet lettre = plateau[i][j].getLettre();
+
+        if (lettre == null) { // si la case est vide, on affiche un carré vide.
+          System.out.print("□ ");
         }
         else {
-        	System.out.print(bonus.toString());
+          System.out.print(lettre + " ");
         }
       }
 
       // Retour à la ligne.
       System.out.println();
     }
-  }
-  
-  public void placerlettre(LettreAlphabetFrancais lettre,int x, int y) throws HorsPlateauException {
-	  this.plateau[y][x].setLettre(lettre);
   }
 }

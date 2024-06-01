63562d013a5e84410b679dad36d4694f495ebb70
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-01 18:45:56 +0200

    feat(plateau/case): ajout de cas généraux pour la vue du case du plateau

--- src/main/java/scrabble/vues/PlateauCaseVue.java
+++ src/main/java/scrabble/vues/PlateauCaseVue.java
@@ -1,49 +1,62 @@
 package scrabble.vues;
 
 import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.FontWeight;
 import scrabble.gui.utils.PoliceTexteUtilitaire;
 
 public class PlateauCaseVue extends Pane {
-    private final String lettre;
-    private final int points;
+    private final String placeholder;
 
-    private static final int TUILE_TAILLE_PX = 52;
+    private String lettre;
+    private final Label contenuLabel;
+    private int points = -1;
+    private final Label pointsLabel;
 
-    public PlateauCaseVue (String lettre, int points) {
+    public static final Color LD_COULEUR = Color.rgb(87, 149, 191);
+    public static final Color MT_COULEUR = Color.rgb(190, 81, 66);
+    public static final Color LT_COULEUR = Color.rgb(8, 85, 150);
+    public static final Color MD_OU_ETOILE_COULEUR = Color.rgb(204, 102, 102);
+
+    /**
+     * Une case spéciale.
+     */
+    public PlateauCaseVue (String placeholder, Color couleurFond) {
         super();
 
-        this.lettre = lettre;
-        this.points = points;
+        // Si jamais la case est spéciale.
+        this.placeholder = placeholder;
 
-        Label lettreLabel = new Label(lettre);
-        lettreLabel.setFont(PoliceTexteUtilitaire.utiliserReadexPro(24));
+        this.contenuLabel = new Label(placeholder);
+        contenuLabel.setFont(PoliceTexteUtilitaire.utiliserReadexPro(24));
         // On centre la lettre dans la tuile en utilisant ses positions.
-        lettreLabel.layoutXProperty().bind(widthProperty().subtract(lettreLabel.widthProperty()).divide(2));
-        lettreLabel.layoutYProperty().bind(heightProperty().subtract(lettreLabel.heightProperty()).divide(2));
+        contenuLabel.layoutXProperty().bind(widthProperty().subtract(contenuLabel.widthProperty()).divide(2));
+        contenuLabel.layoutYProperty().bind(heightProperty().subtract(contenuLabel.heightProperty()).divide(2));
 
-        Label pointsLabel = new Label(String.valueOf(points));
+        this.pointsLabel = new Label();
         pointsLabel.setFont(PoliceTexteUtilitaire.utiliserReadexPro(12, FontWeight.BOLD));
         // On la met en bas à droite de la tuile (décalage de 4px).
         pointsLabel.layoutXProperty().bind(widthProperty().subtract(pointsLabel.widthProperty()).subtract(4));
         pointsLabel.layoutYProperty().bind(heightProperty().subtract(pointsLabel.heightProperty()).subtract(2));
         pointsLabel.setTextFill(Color.rgb(70, 70, 70));
 
-        getChildren().addAll(lettreLabel, pointsLabel);
-
-        setMaxWidth(TUILE_TAILLE_PX);
-        setMaxHeight(TUILE_TAILLE_PX);
-        setMinWidth(TUILE_TAILLE_PX);
-        setMinHeight(TUILE_TAILLE_PX);
+        getChildren().addAll(contenuLabel, pointsLabel);
 
         setBackground(new Background(new BackgroundFill(
-                Color.WHITE,
-                new CornerRadii(4), // 8px (+ 4 pour éviter de passer sur le bord)
-                null
+            couleurFond,
+            new CornerRadii(4), // 8px (+ 4 pour éviter de passer sur le bord)
+            null
         )));
+
+    }
+
+    /**
+     * Une case vide.
+     */
+    public PlateauCaseVue() {
+        this("", Color.WHITE);
     }
 
     public String lettre() {
@@ -52,5 +65,25 @@ public class PlateauCaseVue extends Pane {
 
     public int points() {
         return points;
+    }
+
+    public void assigner(String lettre, int points) {
+        this.lettre = lettre;
+        contenuLabel.setText(lettre);
+
+        this.points = points;
+        pointsLabel.setText(String.valueOf(points));
+    }
+
+    public void vider() {
+        lettre = null;
+        contenuLabel.setText(placeholder);
+
+        points = -1;
+        pointsLabel.setText("");
+    }
+
+    public boolean estAssignee() {
+        return lettre != null && points >= 0;
     }
 }

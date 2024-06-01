5ac4230a74e9f3536e0ea5ce79b9bd2cc54ace74
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-01 18:44:27 +0200

    fix: theme manager
    sinon crash si pas sur win11

--- src/main/java/scrabble/application/ScrabbleFXApplication.java
+++ src/main/java/scrabble/application/ScrabbleFXApplication.java
@@ -1,15 +1,11 @@
 package scrabble.application;
 
+import com.pixelduke.window.ThemeWindowManager;
 import com.pixelduke.window.ThemeWindowManagerFactory;
-import com.pixelduke.window.Win11ThemeWindowManager;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import scrabble.vues.ActionsVue;
 import scrabble.vues.PartieVue;
-import scrabble.vues.PlateauVue;
 
 public class ScrabbleFXApplication extends Application {
     public static void main(String[] args) {
@@ -19,23 +15,16 @@ public class ScrabbleFXApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         PartieVue root = new PartieVue();
-        VBox conteneur = new VBox();
-
-        ActionsVue actions = new ActionsVue();
-        PlateauVue plateau = new PlateauVue();
-        conteneur.getChildren().addAll(plateau, actions);
-        VBox.setVgrow(plateau, Priority.ALWAYS);
-        root.getChildren().add(conteneur);
 
         Scene scene = new Scene(root, 1180, 982);
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
 
         primaryStage.setTitle("Scrabble");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        // On active le mode sombre pour la fenêtre sur Windows 11.
-        Win11ThemeWindowManager themeWindowManager = (Win11ThemeWindowManager) ThemeWindowManagerFactory.create();
+        // On active le mode sombre pour la fenêtre.
+        ThemeWindowManager themeWindowManager = ThemeWindowManagerFactory.create();
         themeWindowManager.setDarkModeForWindowFrame(primaryStage, true);
     }
 }

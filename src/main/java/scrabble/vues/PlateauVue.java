48a82c9f8311114514ee1236409f37f3a6ef21b1
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-01 18:46:24 +0200

    feat(plateau): alignement/redimension automatique du plateau en fonction de la taille du conteneur

--- src/main/java/scrabble/vues/PlateauVue.java
+++ src/main/java/scrabble/vues/PlateauVue.java
@@ -1,20 +1,63 @@
 package scrabble.vues;
 
-import javafx.geometry.Insets;
-import javafx.scene.layout.GridPane;
+import javafx.beans.binding.DoubleExpression;
+import javafx.scene.layout.*;
+import scrabble.model.utils.Coordonee;
 
-public class PlateauVue extends GridPane {
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlateauVue extends Pane {
+    private final Map<Coordonee, PlateauCaseVue> cases = new HashMap<>();
+
     public PlateauVue() {
         super();
 
-        setPadding(new Insets(32, 0, 0, 32));
-        setVgap(2);
-        setHgap(2);
-
         for (int ligne = 0; ligne < 15; ligne++) {
             for (int colonne = 0; colonne < 15; colonne++) {
-                add(new PlateauCaseVue("S", 2), ligne, colonne);
+                PlateauCaseVue casePlateau = new PlateauCaseVue();
+                // On ajoute un écart de 2px entre les cases.
+                DoubleExpression tailleCase = heightProperty().subtract(32 + (2 * 15)).divide(15);
+
+                // On lie la taille de la case en fonction de la taille de la fenêtre.
+                casePlateau.minWidthProperty().bind(tailleCase);
+                casePlateau.maxWidthProperty().bind(tailleCase);
+                casePlateau.minHeightProperty().bind(tailleCase);
+                casePlateau.maxHeightProperty().bind(tailleCase);
+
+                // On récupère la taille de toutes les cases pour les centrer.
+                DoubleExpression tailleToutesLesCases = tailleCase.multiply(15).add(2 * 15);
+
+                // On ajoute horizontalement la case.
+                casePlateau.layoutXProperty().bind(
+                    // Centrage horizontal.
+                    widthProperty().subtract(tailleToutesLesCases).divide(2)
+                    // 2px d'espace entre colonnes.
+                    .add(tailleCase.add(2).multiply(colonne))
+                );
+
+                // On ajoute verticalement la case.
+                casePlateau.layoutYProperty().bind(
+                    // Centrage vertical.
+                    heightProperty().subtract(tailleToutesLesCases).divide(2)
+                    // 2px d'espace entre lignes.
+                    .add(tailleCase.add(2).multiply(ligne))
+                );
+
+                // On ajoute la case dans la hashmap plateau.
+                cases.put(new Coordonee(ligne, colonne), casePlateau);
             }
         }
+
+        getChildren().addAll(cases.values());
+    }
+
+    /**
+     * Récupérer la case située à la coordonnée donnée.
+     * @param coordonee La coordonnée de la case.
+     * @return La case située à la coordonnée donnée.
+     */
+    public PlateauCaseVue caseSitueA(Coordonee coordonee) {
+        return cases.get(coordonee);
     }
 }

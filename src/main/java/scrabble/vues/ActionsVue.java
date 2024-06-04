55c8d16691bd8dd7fa31c377d35a2c5197013731
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-04 19:28:26 +0200

    refactor: changement emplacement du onMouseClicked

--- src/main/java/scrabble/vues/ActionsVue.java
+++ src/main/java/scrabble/vues/ActionsVue.java
@@ -2,50 +2,25 @@ package scrabble.vues;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import scrabble.composants.BoutonMelanger;
 
 public class ActionsVue extends HBox {
+    private final ChevaletVue chevalet;
+
     public ActionsVue() {
         super();
 
-        ChevaletVue chevalet = new ChevaletVue();
+        chevalet = new ChevaletVue();
         setMargin(chevalet, new Insets(0, 0, 0, 32));
 
-        Button melangerBouton = new Button("R ");
-
-        melangerBouton.setMinWidth(48);
-        melangerBouton.setMinHeight(48);
-        melangerBouton.setTranslateX(-48/2);
-        melangerBouton.setTranslateY(10);
-        melangerBouton.setBackground(new Background(new BackgroundFill(
-            Color.WHITE,
-            new CornerRadii(52, true),
-            new Insets(1)
-        )));
-
-        melangerBouton.setOnMouseClicked(event -> {
-            System.out.println("TODO: Mélanger les tuiles du chevalet");
-        });
-
-        melangerBouton.setBorder(new Border(new BorderStroke(
-            Color.rgb(130, 27, 32),
-            BorderStrokeStyle.SOLID,
-            new CornerRadii(50, true),
-            new BorderWidths(2)
-        )));
-
-        melangerBouton.setEffect(new DropShadow(
-            0,
-            0,
-            4,
-            Color.rgb(130, 27, 32)
-        ));
-
+        BoutonMelanger melangerBouton = new BoutonMelanger();
         getChildren().addAll(chevalet, melangerBouton);
 
         setAlignment(Pos.CENTER);
+    }
+
+    public ChevaletVue chevalet() {
+        return chevalet;
     }
 }

7e561c86522b95c1ece8c8f7f637d9023fa28577
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-04 19:27:18 +0200

    feat(Coordonee): ajout méthodes `toString`, `hashCode` et `equals`

--- src/main/java/scrabble/model/utils/Coordonee.java
+++ src/main/java/scrabble/model/utils/Coordonee.java
@@ -1,5 +1,7 @@
 package scrabble.model.utils;
 
+import java.util.Objects;
+
 public class Coordonee {
     private final int ligne;
     private final int colonne;
@@ -15,5 +17,27 @@ public class Coordonee {
 
     public int getColonne() {
         return colonne;
+    }
+
+    @Override
+    public String toString() {
+        return ligne + "," + colonne;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(colonne, ligne);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Coordonee other = (Coordonee) obj;
+        return Objects.equals(colonne, other.colonne) && Objects.equals(ligne, other.ligne);
     }
 }

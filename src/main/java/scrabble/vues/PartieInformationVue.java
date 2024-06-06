896abf7b87633f98967e2105d55b89fad2742408
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-06 09:22:40 +0200

    feat(partie_info): ajout boutons

--- src/main/java/scrabble/vues/PartieInformationVue.java
+++ src/main/java/scrabble/vues/PartieInformationVue.java
@@ -3,21 +3,34 @@ package scrabble.vues;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
-import java.util.Optional;
-
 public class PartieInformationVue extends VBox {
-    private final Button jouerMotBouton;
+    private final Button jouerUnMotBouton;
+    private final Button passerTourBouton;
+    private final Button validerEchangeBouton;
 
     public PartieInformationVue() {
         super();
 
-        jouerMotBouton = new Button("Jouer un mot");
-        getChildren().addAll(new Label("Joueur"), jouerMotBouton);
+        jouerUnMotBouton = new Button("Jouer un mot");
+        passerTourBouton = new Button("Passer son tour");
+        validerEchangeBouton = new Button("Valider l'échange");
+        // On le cache par défaut, doit apparaître seulement lors d'un échange actif.
+        validerEchangeBouton.setVisible(false);
+
+        getChildren().addAll(new Label("Joueur"), jouerUnMotBouton, passerTourBouton, validerEchangeBouton);
         setMinWidth(300);
         setMaxWidth(300);
     }
 
-    public Button recupererJouerMotBouton() {
-        return jouerMotBouton;
+    public Button jouerUnMotBouton() {
+        return jouerUnMotBouton;
+    }
+
+    public Button passerTourBouton() {
+        return passerTourBouton;
+    }
+
+    public Button validerEchangeBouton() {
+        return validerEchangeBouton;
     }
 }

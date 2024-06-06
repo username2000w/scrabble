0d4da4c0bef1c5c793a36ce27d569371d61b4958
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-06 14:00:12 +0200

    feat: ajout du label pour les scores

--- src/main/java/scrabble/vues/PartieInformationVue.java
+++ src/main/java/scrabble/vues/PartieInformationVue.java
@@ -7,6 +7,7 @@ public class PartieInformationVue extends VBox {
     private final Button jouerUnMotBouton;
     private final Button passerTourBouton;
     private final Button validerEchangeBouton;
+    private final Label scoreLabel;
 
     public PartieInformationVue() {
         super();
@@ -14,10 +15,14 @@ public class PartieInformationVue extends VBox {
         jouerUnMotBouton = new Button("Jouer un mot");
         passerTourBouton = new Button("Passer son tour");
         validerEchangeBouton = new Button("Valider l'échange");
+
+        scoreLabel = new Label("0");
+        scoreLabel.setStyle("-fx-text-fill: white; -fx-font-size: 69px; -fx-font-weight: bold; -fx-font-family: 'Arial';");
+
         // On le cache par défaut, doit apparaître seulement lors d'un échange actif.
         validerEchangeBouton.setVisible(false);
 
-        getChildren().addAll(new Label("Joueur"), jouerUnMotBouton, passerTourBouton, validerEchangeBouton);
+        getChildren().addAll(new Label("Joueur"), scoreLabel, jouerUnMotBouton, passerTourBouton, validerEchangeBouton);
         setMinWidth(300);
         setMaxWidth(300);
     }
@@ -32,5 +37,9 @@ public class PartieInformationVue extends VBox {
 
     public Button validerEchangeBouton() {
         return validerEchangeBouton;
+    }
+
+    public Label scoreLabel() {
+        return scoreLabel;
     }
 }

e19a6cb2691ee1adfb8445666f9e409ae446aa56
Léo Gaborit <leo.gaborit@etu.unilim.fr>
2024-06-07 10:18:10 +0200

    tests: ajout classe LettreAlphabetFrancaisTest (coverage 100%)

---
+++ src/test/java/scrabble/test/LettreAlphabetFrancaisTest.java
@@ -0,0 +1,23 @@
+package scrabble.test;
+
+import org.junit.jupiter.api.Test;
+import scrabble.model.LettreAlphabetFrancais;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LettreAlphabetFrancaisTest {
+
+    @Test
+    void testPoints() {
+        // Vérifie que le Joker a bien 0 points
+        assertEquals(1, LettreAlphabetFrancais.A.points());
+        assertEquals(2, LettreAlphabetFrancais.D.points());
+        assertEquals(3, LettreAlphabetFrancais.C.points());
+        assertEquals(4, LettreAlphabetFrancais.F.points());
+        assertEquals(1, LettreAlphabetFrancais.A.points());
+        assertEquals(8, LettreAlphabetFrancais.Q.points());
+        assertEquals(1, LettreAlphabetFrancais.A.points());
+
+    }
+
+}

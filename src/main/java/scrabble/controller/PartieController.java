cbadc0e5a783a96d6e67675fe06af15f5a7d009f
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-09 11:19:38 +0200

    refactor: déplacement du partie controller dans le main

--- src/main/java/scrabble/controller/PartieController.java
+++ src/main/java/scrabble/controller/PartieController.java
@@ -1,6 +1,5 @@
 package scrabble.controller;
 
-import scrabble.gui.utils.ChevaletVueUtilitaire;
 import scrabble.model.*;
 import scrabble.vues.PartieVue;
 
@@ -11,10 +10,5 @@ public class PartieController {
     public PartieController (Plateau plateau, Joueur joueur, Sac sac, PartieVue root) {
         this.root = root;
         this.joueur = joueur;
-
-        // On remplit le sac du joueur au début de la partie.
-        joueur.remplirChevalet(sac);
-        // On actualise le contenu dans l'IHM.
-        ChevaletVueUtilitaire.rafraichirContenu(root, joueur);
     }
 }

d52f8fbb463c2403bc80294d6221e6b42608835b
Maxime <maximechagnon00@gmail.com>
2024-06-09 17:38:38 +0200

    feat(test): dictionaire

---
+++ src/test/java/scrabble/test/DictionaireTest.java
@@ -0,0 +1,24 @@
+package scrabble.test;
+
+import org.junit.jupiter.api.Test;
+import scrabble.model.utils.DictionaireFrancais;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DictionaireTest {
+
+    @Test
+    void mot_dgshsg_pas_dans_le_dictionaire() {
+        DictionaireFrancais dictionaireFrancais = new DictionaireFrancais();
+        // Teste si le mot "dgshsg" n'est pas dans le dictionaire
+        assertFalse(dictionaireFrancais.contient("dgshsg"));
+    }
+
+    @Test
+    void mot_pates_dans_le_dictionaire() {
+        DictionaireFrancais dictionaireFrancais = new DictionaireFrancais();
+        // Teste si le mot "abc" est dans le dictionaire
+        assertTrue(dictionaireFrancais.contient("pates"));
+    }
+}

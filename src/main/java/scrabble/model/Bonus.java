91ac81b7b32ab9149e06e1dcdc21a0aafa84de3b
Maxime <maximechagnon00@gmail.com>
2024-06-10 08:16:46 +0200

    fix: lettre minuscule pour les bonus

--- src/main/java/scrabble/model/Bonus.java
+++ src/main/java/scrabble/model/Bonus.java
@@ -3,8 +3,8 @@ package scrabble.model;
 public enum Bonus {
 	MOT_TRIPLE("3 ", 1, 3),
 	MOT_DOUBLE("2 ", 1, 2),
-	LETTRE_DOUBLE("D ", 2, 1),
-	LETTRE_TRIPLE("T ", 3, 1),
+	LETTRE_DOUBLE("d ", 2, 1),
+	LETTRE_TRIPLE("t ", 3, 1),
 	ETOILE("★ ", 1, 2),;
 	
 	private final String symbole;
